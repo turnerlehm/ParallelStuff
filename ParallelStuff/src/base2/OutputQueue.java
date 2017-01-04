9b823a67a96c7a72e50e84961a8513512d26cb81
root <root@turner-ThinkPad-X260>
2017-01-04 14:08:26 -0800

    Added sorting/buffered output.

--- ParallelStuff/src/base2/OutputQueue.java
+++ ParallelStuff/src/base2/OutputQueue.java
@@ -3,13 +3,18 @@ package base2;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
 
 public class OutputQueue 
 {
 	private String[] Q;
-	private static final String WIN_PATH = "C:\\Users\\Turner\\Destkop\\COMPUTE_RESULTS\\base2_concat";
+	private static final String WIN_PATH = "G:\\Users\\Turner\\Destkop\\COMPUTE_RESULTS\\base2_concat";
 	private static final String UNIX_PATH = "/home/turner/COMPUTE_RESULTS/base2_concat";
 	private BufferedWriter out;
+	private boolean cycling, broken;
+	private int left, qidx = 0;
+	
 	public OutputQueue(int threads)
 	{
 		Q = new String[threads];
@@ -23,6 +28,9 @@ public class OutputQueue
 				out = new BufferedWriter(new FileWriter(UNIX_PATH + fname));
 			else
 				out = new BufferedWriter(new FileWriter(WIN_PATH + fname));
+			left = threads;
+			cycling = false; 
+			broken  = false;
 		}
 		catch(IOException ioe)
 		{
@@ -44,6 +52,9 @@ public class OutputQueue
 				out = new BufferedWriter(new FileWriter(UNIX_PATH + fname));
 			else
 				out = new BufferedWriter(new FileWriter(WIN_PATH + fname));
+			left = threads;
+			cycling = false; 
+			broken  = false;
 		}
 		catch(IOException ioe)
 		{
@@ -65,6 +76,9 @@ public class OutputQueue
 				out = new BufferedWriter(new FileWriter(UNIX_PATH + fname));
 			else
 				out = new BufferedWriter(new FileWriter(WIN_PATH + fname));
+			left = threads;
+			cycling = false; 
+			broken  = false;
 		}
 		catch(IOException ioe)
 		{
@@ -80,6 +94,9 @@ public class OutputQueue
 		try
 		{
 			out = new BufferedWriter(new FileWriter(path + fname));
+			left = threads;
+			cycling = false; 
+			broken  = false;
 		}
 		catch(IOException ioe)
 		{
@@ -95,6 +112,9 @@ public class OutputQueue
 		try
 		{
 			out = new BufferedWriter(new FileWriter(path + fname));
+			left = threads;
+			cycling = false; 
+			broken  = false;
 		}
 		catch(IOException ioe)
 		{
@@ -110,11 +130,73 @@ public class OutputQueue
 		try
 		{
 			out = new BufferedWriter(new FileWriter(path + fname));
+			left = threads;
+			cycling = false; 
+			broken  = false;
 		}
 		catch(IOException ioe)
 		{
 			System.err.println(ioe.getStackTrace());
 			System.exit(-1);
 		}
+	}
+	
+	public synchronized int await()throws InterruptedException, BrokenBarrierException
+	{
+		int idx = left - 1;
+		if(!cycling)
+			cycling = !cycling;
+		if(left > 1)
+		{
+			left--;
+			while(cycling)
+			{
+				try
+				{
+					wait();
+				}
+				catch(InterruptedException ie)
+				{
+					broken = !broken;
+					notifyAll();
+					throw new InterruptedException();
+				}
+				if(broken)
+				{
+					notifyAll();
+					throw new BrokenBarrierException();
+				}
+			}
+		}
+		else
+		{
+			left = Q.length;
+			qidx = 0;
+			Arrays.sort(Q);
+			for(String S : Q)
+			{
+				try
+				{
+					out.write(S);
+					out.newLine();
+					out.flush();
+				}
+				catch(IOException ioe)
+				{
+					notifyAll();
+					throw new BrokenBarrierException();
+				}
+			}
+			cycling = !cycling;
+			notifyAll();
+			return idx;
+		}
+		return idx;
+	}
+	
+	public synchronized void insert(String s)
+	{
+		Q[qidx] = s;
+		qidx++;
 	}
 }
