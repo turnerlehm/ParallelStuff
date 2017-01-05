29e9a0760441bd65b98b8d4574553b36ce6a91a9
Turner Lehmbecker <edmfrosty@gmail.com>
2017-01-05 13:06:38 -0800

    laying down the foundations

--- ParallelStuff/src/base2/Base2Threadv1.java
+++ ParallelStuff/src/base2/Base2Threadv1.java
@@ -1,9 +1,33 @@
 package base2;
 
+import java.math.BigInteger;
+import java.util.concurrent.BrokenBarrierException;
+
 public class Base2Threadv1 extends Thread
 {
-	public Base2Threadv1()
+	private int start, end, step;
+	private OutputQueue Q;
+	public Base2Threadv1(int start, int end, int step, OutputQueue Q)
 	{
-		
+		this.start = start;
+		this.end = end;
+		this.step = step;
+		this.Q = Q;
+	}
+	
+	public void run()
+	{
+		for(int i = start; i <= end; i += step)
+		{
+			//1. translate to concatenated powers of 2 expansion
+			BigInteger N = new BigInteger("" + i);
+			Q.insert(N.toString());
+			try {
+				Q.await();
+			} catch (InterruptedException | BrokenBarrierException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
